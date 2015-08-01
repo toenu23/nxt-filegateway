751241caeed692392614a2a370b624ec79ced3af
toenu23 <fubarphukhew@gmail.com>
2015-08-01 12:54:59 +0200

    basic app structure

---
+++ app.js
@@ -0,0 +1,32 @@
+// Requirements
+var http = require('http');
+var express = require('express');
+
+var config = require('./lib/config');
+var nxt = require('./lib/nxt');
+
+// HTTP Server
+var app = express();
+app.set('port', 2332);
+var server = http.createServer(app);
+server.listen(app.get('port'), function(err, result) {
+    console.log('Server listening on port ' + app.get('port'));
+});
+
+
+// Routes
+app.get('/', function(req, res, next) {
+
+});
+
+app.get('/tag/*', function(req, res, next) {
+
+});
+
+app.get('/channel/*', function(req, res, next) {
+
+});
+
+app.get('/file/*', function(req, res, next) {
+
+});
