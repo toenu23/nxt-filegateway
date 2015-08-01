0c4aa59164513b188dca29f720d59b93ed943a7d
toenu23 <fubarphukhew@gmail.com>
2015-08-01 19:12:08 +0200

    controller

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+(function() {
+
+  var dependencies = [
+  ];
+
+  var app = angular.module('FileApp', dependencies);
+
+  var mainController = function($scope, $timeout) {
+
+  };
+
+  var mainControllerArgs = [
+    '$scope',
+    '$timeout',
+    mainController,
+  ];
+
+  app.controller('MainController', mainControllerArgs);
+
+})();
+
